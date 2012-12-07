4f1125239e5b618864acb90be2f6897fa4ffb7cb
jovmaster <jmartis4@gmail.com>
2012-12-07 01:26:00 +0100

    eerste verandering

--- Java/src/Hoofdstuk1/h01.java
+++ Java/src/Hoofdstuk1/h01.java
@@ -1,6 +1,29 @@
 package Hoofdstuk1;
 
+import javax.swing.*;
+
+
+
 public class h01
 {
+	
+	JFrame venster = null;
+	
+	public h01()
+	{
+		JFrame venster = new JFrame();
+		venster.setTitle("Hoofdstuk 1 - Opdracht 1");
+		venster.setLocation(100, 100);
+		venster.setSize(300, 300);
+		venster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+	}
+	
+	public static void main (String [] args)
+	{
+		
+		new h01();
+	}
 
 }
